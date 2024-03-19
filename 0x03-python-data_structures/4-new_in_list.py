859c8db309f9e26743075a6791fa7e62a896bf86
FurankieSama <franknwokoma1@gmail.com>
2024-03-19 11:24:03 -0400

    Python-Lists

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def new_in_list(my_list,idx,element):
+def new_in_list(my_list, idx, element):
     temp = my_list[:]
     if 0 <= idx < len(my_list):
         temp[idx] = element
