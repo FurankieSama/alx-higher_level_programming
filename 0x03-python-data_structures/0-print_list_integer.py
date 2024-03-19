469eebb2a7bc3ab9ad94ef473414945cc9a96f88
FurankieSama <franknwokoma1@gmail.com>
2024-03-19 03:10:34 +0100

    Print list of int

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
-def print_list_integer(_list=[]):
-    for i in _list:
-        print('{:d}'.format(i))
+def print_list_integer(my_list=[]):
+ 
+   for i in my_list:
+      print('{}'.format(i))
+
+my_list = range(5)
+print_list_integer(my_list)
