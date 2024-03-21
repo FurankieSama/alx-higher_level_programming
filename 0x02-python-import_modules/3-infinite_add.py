20595f386d1165c8a67ccbe856d9663cc739f71b
FurankieSama <franknwokoma1@gmail.com>
2024-03-20 20:10:57 -0400

    infinite add

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 if __name__ == '__main__':
     import sys
-    summation = 0
+    sum = 0
     argument_list = sys.argv[1:]
     for argument in argument_list:
-        summation += int(argument)
-    print("{}".format(summation))
+        sum += int(argument)
+    print("{}".format(sum))
